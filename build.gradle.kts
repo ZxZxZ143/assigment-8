55ec2c931d5610825fad7449a9c9ecb4185e1d70
ZxZxZ154 <kyrlukov205@gmail.com>
2026-05-12 03:31:51 +0500

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,9 @@ application {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
