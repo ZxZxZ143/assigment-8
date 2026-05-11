b4ed1ce17260713deaacc3f86c436b0127cb148e
ZxZxZ154 <kyrlukov205@gmail.com>
2026-05-12 03:20:32 +0500

    commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,16 @@
-rootProject.name = "assigment8"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "assigment8"
